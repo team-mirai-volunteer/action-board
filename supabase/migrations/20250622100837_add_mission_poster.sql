5dbb6824cfe3aebdbd1c20b8836c3b837be5d48f
gmmtea <47593304+gmmtea@users.noreply.github.com>
2025-06-22 19:39:15 +0900

    ミッション「チームみらいの政党ポスターを貼ろう」を追加 (#636)
    Co-authored-by: gmmtea <>

---
+++ supabase/migrations/20250622100837_add_mission_poster.sql
@@ -0,0 +1,30 @@
+-- ミッション「チームみらいの政党ポスターを貼ろう」を追加
+INSERT INTO missions (
+  id,
+  title,
+  icon_url,
+  content,
+  difficulty,
+  event_date,
+  required_artifact_type,
+  max_achievement_count,
+  ogp_image_url,
+  artifact_label,
+  is_featured,
+  is_hidden
+)
+VALUES (
+  gen_random_uuid(),
+  'チームみらいの政党ポスターを貼ろう',
+  '/img/mission_fallback.svg',
+  'チームみらいの政党ポスターを貼って、地域の人々に政策を広めよう！<br><br>詳しいポスター貼りの方法や注意点については、こちらのページをご確認ください：<br><a href="https://team-mirai.notion.site/206f6f56bae180e6b471ec5360653227" target="_blank">政治ポスターを貼る</a>',
+  4,
+  NULL,
+  'TEXT',
+  NULL,
+  '',
+  'ポスターを貼った場所の郵便番号',
+  FALSE,
+  FALSE
+)
+;
