172e741851be5afbbd437a075b09d31b072eda23
Seiichiro Tanaka <seiichi3141@gmail.com>
2025-06-27 01:37:07 +0900

    #626 ランキング関連コンポーネントのリファクタリング

--- components/ranking/ranking-mission.tsx
+++ components/ranking/ranking-mission.tsx
@@ -1,12 +1,10 @@
 // TOPページ用のランキングコンポーネント
-import { Card } from "@/components/ui/card";
 import {
   getMissionRanking,
   getTopUsersPostingCount,
 } from "@/lib/services/missionsRanking";
 import type { Tables } from "@/lib/types/supabase";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
+import BaseRanking from "./base-ranking";
 import { RankingItem } from "./ranking-item";
 
 interface RankingTopProps {
@@ -52,39 +50,22 @@ export default async function RankingMission({
   };
 
   return (
-    <div className="max-w-6xl mx-auto">
-      <div className="flex flex-col gap-6">
-        <div className="">
-          <h2 className="text-xl md:text-4xl text-gray-900 mb-2 text-center">
-            🏅「{mission.title}」トップ{limit}
-          </h2>
-        </div>
-
-        <Card className="border-2 border-gray-200 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 p-8 bg-white">
-          <div className="space-y-1">
-            {rankings.map((user) => (
-              <RankingItem
-                key={user.user_id}
-                user={user}
-                userWithMission={user}
-                mission={
-                  mission ? { id: mission.id, name: mission.title } : undefined
-                }
-                badgeText={badgeText(user.user_id || "")}
-              />
-            ))}
-          </div>
-        </Card>
-        {showDetailedInfo && (
-          <Link
-            href={`/ranking/ranking-mission?missionId=${mission.id}`}
-            className="flex items-center text-teal-600 hover:text-teal-700 self-center"
-          >
-            トップ100を見る
-            <ChevronRight className="w-4 h-4 ml-1" />
-          </Link>
-        )}
-      </div>
-    </div>
+    <BaseRanking
+      title={`🏅「${mission.title}」トップ${limit}`}
+      detailsHref={`/ranking/ranking-mission?missionId=${mission.id}`}
+      showDetailedInfo={showDetailedInfo}
+    >
+      {rankings.map((user) => (
+        <RankingItem
+          key={user.user_id}
+          user={user}
+          userWithMission={user}
+          mission={
+            mission ? { id: mission.id, name: mission.title } : undefined
+          }
+          badgeText={badgeText(user.user_id || "")}
+        />
+      ))}
+    </BaseRanking>
   );
 }
--- components/ranking/ranking-top.tsx
+++ components/ranking/ranking-top.tsx
@@ -1,8 +1,6 @@
 // TOPページ用のランキングコンポーネント
-import { Card } from "@/components/ui/card";
 import { getRanking } from "@/lib/services/ranking";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
+import BaseRanking from "./base-ranking";
 import { RankingItem } from "./ranking-item";
 
 interface RankingTopProps {
@@ -17,31 +15,14 @@ export default async function RankingTop({
   const rankings = await getRanking(limit);
 
   return (
-    <div className="max-w-6xl mx-auto">
-      <div className="flex flex-col gap-6">
-        <div className="">
-          <h2 className="text-2xl md:text-4xl text-gray-900 mb-2 text-center">
-            🏅アクションリーダートップ{limit}
-          </h2>
-        </div>
-
-        <Card className="border-2 border-gray-200 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 p-8 bg-white">
-          <div className="space-y-1">
-            {rankings.map((user) => (
-              <RankingItem key={user.user_id} user={user} />
-            ))}
-          </div>
-        </Card>
-        {showDetailedInfo && (
-          <Link
-            href="/ranking"
-            className="flex items-center text-teal-600 hover:text-teal-700 self-center"
-          >
-            トップ100を見る
-            <ChevronRight className="w-4 h-4 ml-1" />
-          </Link>
-        )}
-      </div>
-    </div>
+    <BaseRanking
+      title={`🏅アクションリーダートップ${limit}`}
+      detailsHref="/ranking"
+      showDetailedInfo={showDetailedInfo}
+    >
+      {rankings.map((user) => (
+        <RankingItem key={user.user_id} user={user} />
+      ))}
+    </BaseRanking>
   );
 }
