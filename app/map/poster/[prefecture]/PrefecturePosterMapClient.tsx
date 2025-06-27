22459057a3fa17ca7f3ab8ecf5594e602815c756
Shota Shimizu <tano.sukenyabu@gmail.com>
2025-06-27 15:35:57 +0900

    fix build

--- app/map/poster/[prefecture]/PrefecturePosterMapClient.tsx
+++ app/map/poster/[prefecture]/PrefecturePosterMapClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
