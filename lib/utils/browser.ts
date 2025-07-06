165989ea564efaa3fc7bb66cf059d59c4cd959f0
sakurai <>
2025-07-06 19:27:49 +0900

    fix 不要なファイルを削除

--- lib/utils/browser.ts
+++
@@ -1,7 +0,0 @@
-export function getOrigin(): string {
-  return typeof window !== "undefined" ? window.location.origin : "";
-}
-
-export function getCurrentUrl(): string {
-  return typeof window !== "undefined" ? window.location.href : "";
-}
