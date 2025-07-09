798f4ab40a29c9af72bed4386183d5886a3ae41a
a-puriy <y082pmt@gmail.com>
2025-07-09 17:34:24 +0900

    test: add coverage budge

--- jest.config.js
+++ jest.config.js
@@ -31,6 +31,7 @@ const config = {
     "<rootDir>/lib/constants.ts",
   ],
   coverageProvider: "v8",
+  reporters: ["default", "jest-junit"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
