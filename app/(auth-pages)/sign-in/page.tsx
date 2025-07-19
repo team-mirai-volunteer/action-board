1913f0f667a65a5c18592f057598c88e72ad9971
gmmtea <>
2025-07-19 15:13:05 +0900

    ロゴを白背景用のものに変更

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -12,7 +12,7 @@ export default async function Login(props: {
   return (
     <div className="flex-1 flex flex-col min-w-72">
       <div className="flex justify-center items-center m-4">
-        <Image src="/img/logo.png" alt="logo" width={114} height={96} />
+        <Image src="/img/logo_shiro.png" alt="logo" width={114} height={96} />
       </div>
       <h1 className="text-2xl font-medium text-center mb-2">ログイン</h1>
       <p className="text-sm text-foreground text-center">
--- app/(auth-pages)/sign-up-email/page.tsx
+++ app/(auth-pages)/sign-up-email/page.tsx
@@ -10,7 +10,7 @@ export default async function EmailSignup(props: {
   return (
     <div className="flex-1 flex flex-col min-w-72">
       <div className="flex justify-center items-center m-4">
-        <Image src="/img/logo.png" alt="logo" width={114} height={96} />
+        <Image src="/img/logo_shiro.png" alt="logo" width={114} height={96} />
       </div>
       <EmailSignUpForm searchParams={searchParams} />
     </div>
