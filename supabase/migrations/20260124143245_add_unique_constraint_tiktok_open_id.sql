8c9a7ab1eb0e49424d27c0c3eae4d878193d0394
Shota Shimizu <tano.sukenyabu@gmail.com>
2026-01-25 11:00:57 +0900

    Revert "同じTikTokアカウントを複数のユーザーが連携できないように (#1726)"
    This reverts commit 0281dc23191fe8df9063bb46a56cef50285784b6.

--- supabase/migrations/20260124143245_add_unique_constraint_tiktok_open_id.sql
+++
@@ -1,4 +0,0 @@
--- tiktok_open_idにユニーク制約を追加
--- 同じTikTokアカウントを複数のユーザーが連携できないようにする
-ALTER TABLE tiktok_user_connections
-ADD CONSTRAINT tiktok_user_connections_tiktok_open_id_unique UNIQUE (tiktok_open_id);
